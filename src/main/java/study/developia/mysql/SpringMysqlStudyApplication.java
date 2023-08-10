603e732b292e5f664a5454ee8251ae31aa6f0d2c
developiaa <developiaa@gmail.com>
2023-08-10 22:56:57 +0900

    first commit

---
+++ src/main/java/study/developia/mysql/SpringMysqlStudyApplication.java
@@ -0,0 +1,13 @@
+package study.developia.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlStudyApplication.class, args);
+    }
+
+}
